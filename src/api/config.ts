1a282186d86ea1ce8e5cf26dac0097e85808c082
Petr Cermak <pcermak@multima.cz>
2025-05-17 22:20:10 +0200

    initial commit

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,4 +1,8 @@
 export const API_BASE = import.meta.env.VITE_API_BASE;
+
+if (!API_BASE) {
+  throw new Error("❌ VITE_BACKEND_URL není definován. Přidej ho do .env nebo Netlify environment variables.");
+}
 export default {
   API_BASE,
 };
